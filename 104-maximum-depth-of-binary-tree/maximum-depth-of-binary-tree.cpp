f8393ee26695ad024b331a4f8c622d12536f2f28
Shivkumar Singh Rajawat <113820989+rajawatshivpratap46@users.noreply.github.com>
2026-02-20 22:10:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (47.66%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -14,41 +14,52 @@ public:
     int maxDepth(TreeNode* root) {
         
         //Approach-1 BFS Solution
+        // if(root==nullptr){
+        //     return 0;
+        // }
+        
+        // queue<TreeNode*> mp;
+        // mp.push(root);
+
+        // //vector<vector<int>>res;
+        // int Currentlevel=0;
+
+        // while(!mp.empty()){
+            
+        //     int len=mp.size();
+        //     //TreeNode* node= mp.front();
+        //     //mp.pop();
+
+        //    for(int i=0;i<len;i++){
+
+        //        TreeNode* node= mp.front();
+        //        mp.pop();
+        //       //res[Currentlevel].push_back(node->val);
+
+        //       if(node->left!=nullptr){
+        //         mp.push(node->left);
+        //       }
+
+        //       if(node->right!=nullptr){
+        //         mp.push(node->right);
+        //       }
+
+        //    }
+        //    Currentlevel++;
+        // }
+        // //int result=res.size();
+
+        // return Currentlevel;
+
+        //Approach-2 DFS Solution Recursive
+        
         if(root==nullptr){
             return 0;
         }
-        
-        queue<TreeNode*> mp;
-        mp.push(root);
 
-        //vector<vector<int>>res;
-        int Currentlevel=0;
+        int left= maxDepth(root->left);
+        int right=maxDepth(root->right);
 
-        while(!mp.empty()){
-            
-            int len=mp.size();
-            //TreeNode* node= mp.front();
-            //mp.pop();
-
-           for(int i=0;i<len;i++){
-
-               TreeNode* node= mp.front();
-               mp.pop();
-              //res[Currentlevel].push_back(node->val);
-
-              if(node->left!=nullptr){
-                mp.push(node->left);
-              }
-
-              if(node->right!=nullptr){
-                mp.push(node->right);
-              }
-
-           }
-           Currentlevel++;
-        }
-        //int result=res.size();
-
-        return Currentlevel;
+        return max(left,right)+1;
     }
 };
